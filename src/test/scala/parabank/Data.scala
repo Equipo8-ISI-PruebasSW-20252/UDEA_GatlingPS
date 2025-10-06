45c59fd62678d5b7e65dad1c54cd89c7bc69a0d5
Alexander Valencia Delgado <106996382+alexvadelgado@users.noreply.github.com>
2025-10-06 15:16:22 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,5 +4,9 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "albert"
     val password = "123"
-
+    val fromAccountId = "14343"
+    val toAccountId = "14565"
+    val amount = "10"
+ 
 }
+
